1742de1e3077c5cc3107d887f85ac2748daebe62
AakasH <207394387+Aak4shz@users.noreply.github.com>
2026-08-21 22:14:56 +0530

    Update and add iso information and image tools lines & commnads

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -18,11 +18,15 @@ airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' 
 bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
+  ["/etc/gshadow"]="0:0:400"
+  ["/etc/sudoers.d"]="0:0:750"
+  ["/etc/sudoers.d/umbrella"]="0:0:440"
   ["/root"]="0:0:750"
   ["/root/.automated_script.sh"]="0:0:755"
   ["/root/.gnupg"]="0:0:700"
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
+  ["/usr/local/bin/umbrella-live-init.sh"]="0:0:755"
   ["/usr/local/bin/umbrella-post-install.sh"]="0:0:755"
 )
